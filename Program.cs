6a6df28f9382124bf6502b85d0b47377872ed186
Minsetr0 <nesterovmaksim71@mail.ru>
2023-04-22 16:29:11 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿Random random = new Random();
+int minRandomNumber = 0;
+int maxRandomNumber = 100;
+int numbersInArray = 40;
+int[] numbers = new int[numbersInArray];
+int[] tempNumbers = new int[numbersInArray];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = random.Next(minRandomNumber, maxRandomNumber);
+}
+
+for (int j = 0; j < numbers.Length; j++)
+{
+    for (int i = 0; i < numbers.Length - 1; i++)
+    {
+        if (numbers[i] > numbers[i + 1])
+        {
+            tempNumbers[i] = numbers[i];
+            numbers[i] = numbers[i + 1];
+            numbers[i + 1] = tempNumbers[i];
+        }
+    }
+}
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write(numbers[i] + " ");
+}
